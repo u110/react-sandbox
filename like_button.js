f05750dcaafab44e01bed69a5c7f0c8a86780388
u110 <web.u.project@gmail.com>
2020-10-11 22:06:21 +0900

    ヒント：コンポーネントを再利用する
    https://ja.reactjs.org/docs/add-react-to-a-website.html#tip-reuse-a-component

--- like_button.js
+++ like_button.js
@@ -10,7 +10,7 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      return 'You liked this.';
+      return 'You liked comment number ' + this.props.commentID;
     }
 
     return e(
@@ -21,5 +21,11 @@ class LikeButton extends React.Component {
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+document.querySelectorAll('.like_button_container')
+  .forEach(domContainer => {
+    const commentID = parseInt(domContainer.dataset.commentid, 10);
+    ReactDOM.render(
+      e(LikeButton, { commentID: commentID }),
+      domContainer
+    );
+  });
