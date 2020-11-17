141e805c707d43f9edc493ad7d9eed94bd809828
u110 <web.u.project@gmail.com>
2020-11-17 23:03:46 +0900

    filter button

--- mytodo/src/App.tsx
+++ mytodo/src/App.tsx
@@ -13,6 +13,10 @@ export const App: React.FC = () => {
     { title: "reactペアプロ", done: false },
     { title: "リリース確認", done: true },
   ]);
+
+  const [filterTarget, setFilterTarget] = useState<"all" | "active" | "done">(
+    "all"
+  );
   return (
     <>
       <div>my todo</div>
@@ -21,27 +25,63 @@ export const App: React.FC = () => {
           setTodos([...todos, { title: v, done: false }]);
         }}
       ></TodoInput>
-      {todos.map((todo, i) => {
-        return (
-          <TodoItem
-            todo={todo}
-            key={i}
-            toggleItem={() => {
-              setTodos(
-                todos.map((td, j) => {
-                  if (i === j) {
-                    return { title: td.title, done: !td.done };
-                  }
-                  return td;
-                })
-              );
-            }}
-            removeItem={() => {
-              setTodos(todos.filter((_, j) => i !== j));
-            }}
-          ></TodoItem>
-        );
-      })}
+      <div>
+        <button
+          onClick={() => {
+            setFilterTarget("all");
+          }}
+          style={filterTarget === "all" ? { color: "#f00" } : {}}
+        >
+          ALL
+        </button>
+        <button
+          onClick={() => {
+            setFilterTarget("active");
+          }}
+          style={filterTarget === "active" ? { color: "#f00" } : {}}
+        >
+          Active
+        </button>
+        <button
+          onClick={() => {
+            setFilterTarget("done");
+          }}
+          style={filterTarget === "done" ? { color: "#f00" } : {}}
+        >
+          Done
+        </button>
+      </div>
+      {todos
+        .filter((todo) => {
+          if (filterTarget === "active") {
+            return !todo.done;
+          }
+          if (filterTarget === "done") {
+            return todo.done;
+          }
+          return true;
+        })
+        .map((todo, i) => {
+          return (
+            <TodoItem
+              todo={todo}
+              key={i}
+              toggleItem={() => {
+                setTodos(
+                  todos.map((td, j) => {
+                    if (i === j) {
+                      return { title: td.title, done: !td.done };
+                    }
+                    return td;
+                  })
+                );
+              }}
+              removeItem={() => {
+                setTodos(todos.filter((_, j) => i !== j));
+              }}
+            ></TodoItem>
+          );
+        })}
     </>
   );
 };
